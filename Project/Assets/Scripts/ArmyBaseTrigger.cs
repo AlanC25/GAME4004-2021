9b7c02ec8ccabe8440577cf28429845a855a8d4b
AlanC25 <alan.coleman25@gmail.com>
2020-05-26 16:04:30 +0100

    Drive level complete

---
+++ Project/Assets/Scripts/ArmyBaseTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ArmyBaseTrigger : MonoBehaviour
+{
+    public string levelToLoad;
+    public GameObject hoverCar, armyBase;
+    Collider carCollider, baseCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (hoverCar != null)
+            carCollider = hoverCar.GetComponent<Collider>();
+
+        if (armyBase != null)
+            baseCollider = armyBase.GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (baseCollider.bounds.Intersects(carCollider.bounds))
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
---
+++ Project/Assets/Scripts/Text.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+    //Add reference to UI Text here via the inspector
+    public Text textToAppear;  
+
+    private float timeToAppear = 2f;
+    private float timeWhenDisappear;
+
+    //Call to enable the text, which also sets the timer
+    public void EnableText()
+    {
+        textToAppear.enabled = true;
+        timeWhenDisappear = Time.time + timeToAppear;
+    }
+
+    //We check every frame if the timer has expired and the text should disappear
+    void Update()
+    {
+        if (textToAppear.enabled && (Time.time >= timeWhenDisappear))
+        {
+            textToAppear.enabled = false;
+        }
+    }
+}
